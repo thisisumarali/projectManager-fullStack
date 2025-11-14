cf63b8773cbe11febc9fa900a7e1ea35ea9dee38
Umar khan <umeralikhan7864@gmail.com>
2025-11-14 18:40:27 +0500

    AuthStore

---
+++ client/store/auth.jsx
@@ -0,0 +1,49 @@
+"use client";
+import { createContext, useContext, useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  // Check token when app loads
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      try {
+        const decoded = jwtDecode(token);
+        setUser(decoded);
+      } catch (err) {
+        console.error("Invalid token:", err);
+        localStorage.removeItem("token");
+        setUser(null);
+      }
+    }
+  }, []);
+
+  // Login function
+  const login = (token) => {
+    try {
+      const decoded = jwtDecode(token);
+      localStorage.setItem("token", token);
+      setUser(decoded);
+    } catch (err) {
+      console.error("Error decoding token:", err);
+    }
+  };
+
+  // Logout function
+  const logout = () => {
+    localStorage.removeItem("token");
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
